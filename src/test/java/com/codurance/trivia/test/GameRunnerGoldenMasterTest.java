1357b89d7dc2438b9ff92cd32b864e29616e91fe
Tom <thomas.spencer@codurance.com>
2021-05-09 10:20:19 +0100

    Add save test for file

--- src/test/java/com/codurance/trivia/test/GameRunnerGoldenMasterTest.java
+++ src/test/java/com/codurance/trivia/test/GameRunnerGoldenMasterTest.java
@@ -1,13 +1,25 @@
 package com.codurance.trivia.test;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Random;
 
 public class GameRunnerGoldenMasterTest {
 
   private static boolean notAWinner;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    // REFACTOR Make current working directory more explicit?
+    final File testRunOutputFile = new File("test-data", "golden-master.txt");
+    final PrintWriter canvas = new PrintWriter(new FileWriter(testRunOutputFile));
+    canvas.println("Hello, world!");
+    canvas.flush();
+    canvas.close();
+
+
     Game aGame = new Game();
 
     aGame.add("Chet");
