1a96a236ad44647f3dcfecee0ecb6f719a804c34
Tom <thomas.spencer@codurance.com>
2021-05-11 20:00:01 +0100

    Add test for GameRunnerGoldenmaster

--- src/test/java/com/codurance/trivia/test/GameRunnerGoldenMasterTest.java
+++ src/test/java/com/codurance/trivia/test/GameRunnerGoldenMasterTest.java
@@ -1,22 +1,25 @@
 package com.codurance.trivia.test;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import com.adaptionsoft.games.uglytrivia.Game;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.Random;
+import org.junit.Test;
 
 public class GameRunnerGoldenMasterTest {
 
   private static boolean notAWinner;
 
-  public static void main(String[] args) throws IOException {
+  @Test
+  public void game_works() throws IOException {
     final PrintStream systemOut = System.out;
 
     // REFACTOR Make current working directory more explicit?
-    final File testRunOutputFile = new File("test-data", "golden-master.txt");
+    final File testRunOutputFile = new File("test-data", "test-run.txt");
 
     System.setOut(new PrintStream(testRunOutputFile));
     Game aGame = new Game();
@@ -38,5 +41,13 @@ public class GameRunnerGoldenMasterTest {
 
     } while (notAWinner);
     System.setOut(systemOut);
+
+    File test = new File("test-data", "test-run.txt");
+    File master = new File("test-data", "golden-master.txt");
+
+    byte[] first = Files.readAllBytes(test.toPath());
+    byte[] second = Files.readAllBytes(master.toPath());
+    assertArrayEquals(first, second);
   }
+
 }
